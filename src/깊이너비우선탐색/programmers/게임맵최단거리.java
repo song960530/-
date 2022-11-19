a916bb62e8746666b99a792005828718a2368d34
SongMunJun <song960530@naver.com>
2022-11-19 15:50:46 +0900

    feat: programmers dfa/bfs sol

---
+++ src/깊이너비우선탐색/programmers/게임맵최단거리.java
@@ -0,0 +1,53 @@
+package 깊이너비우선탐색.programmers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 게임맵최단거리 {
+    public static void main(String[] args) {
+        int result1 = new Solution().solution(new int[][]{{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}});
+        int result2 = new Solution().solution(new int[][]{{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 0}, {0, 0, 0, 0, 1}});
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+    static class Solution {
+        public int solution(int[][] maps) {
+            int[][] visit = new int[maps.length][maps[0].length];
+            Queue<int[]> queue = new LinkedList<>();
+            queue.offer(new int[]{0, 0});
+            visit[0][0] = 1;
+
+            while (!queue.isEmpty()) {
+                bfs(maps, queue, visit);
+            }
+
+            int result = visit[maps.length - 1][maps[0].length - 1];
+
+            return result == 0 ? -1 : result;
+        }
+
+        private void bfs(int[][] maps, Queue<int[]> queue, int[][] visit) {
+            int[] position = queue.poll();
+            int x = position[0];
+            int y = position[1];
+
+            // 시계 방향
+            int[] dx = {1, 0, -1, 0};
+            int[] dy = {0, 1, 0, -1};
+
+            for (int i = 0; i < 4; i++) {
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+
+                if (newX < 0 || newX >= maps.length || newY < 0 || newY >= maps[0].length)
+                    continue;
+
+                if (maps[newX][newY] == 1 && visit[newX][newY] == 0) {
+                    visit[newX][newY] = visit[x][y] + 1;
+                    queue.offer(new int[]{newX, newY});
+                }
+            }
+        }
+    }
+}
---
+++ src/깊이너비우선탐색/programmers/타겟넘버.java
@@ -0,0 +1,30 @@
+package 깊이너비우선탐색.programmers;
+
+public class 타겟넘버 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new int[]{1, 1, 1, 1, 1},3));
+        System.out.println(new Solution().solution(new int[]{4, 1, 2, 1},4));
+    }
+
+    static class Solution {
+        private int count = 0;
+
+        public int solution(int[] numbers, int target) {
+
+            dfs(numbers, 0, 0, target);
+
+            return count;
+        }
+
+        private void dfs(int[] numbers, int depth, int sum, int target) {
+            if (numbers.length == depth) {
+                if(sum == target) this.count++;
+            } else {
+                dfs(numbers,depth+1,sum+numbers[depth],target);
+                dfs(numbers,depth+1,sum-numbers[depth],target);
+            }
+        }
+    }
+}
+
+
